547af54add40d23702cf24f675485319792000a5
Jayson Grace <jayson.e.grace@gmail.com>
2022-08-23 20:04:23 -0600

    Initial commit

---
+++ .hooks/go-release-notes.md
@@ -0,0 +1,3 @@
+<!-- markdownlint-disable-file MD041 -->
+
+**Full Changelog**: <https://github.com/l50/goproject/compare/v1.0.0...v1.0.1>
---
+++ .hooks/go-vet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+pkg=$(go list ./...)
+for dir in */; do
+    if [[ "${dir}" != ".mage" ]] \
+                              && [[ "${dir}" != "config/" ]] \
+                              && [[ "${dir}" != "bin/" ]] \
+                           && [[ "${dir}" != "resources/" ]] \
+                                 && [[ "${dir}" != "docs/" ]] \
+                            && [[ "${dir}" != "files/" ]] \
+                             && [[ "${dir}" != "logs/" ]]; then
+        go vet "${pkg}/${dir}"
+    fi
+done
