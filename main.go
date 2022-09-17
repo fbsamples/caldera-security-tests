4f121c9ebfc98c4ba647e9db75307e76fbc4a8a6
Jayson Grace <jayson.e.grace@gmail.com>
2022-09-16 18:10:51 -0600

    Update copyright

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 Jayson Grace <jayson.e.grace@gmail.com>
+Copyright © 2022-present, Meta, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
