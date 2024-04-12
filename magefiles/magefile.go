c06611d89e16e57b65b8b33e927cf3198223c93b
Jayson Grace <jayson.e.grace@gmail.com>
2024-04-12 16:01:49 -0600

    Add missing copyright headers; remove old hook

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,5 +1,24 @@
 //go:build mage
 
+/*
+Copyright © 2024-present, Meta Platforms, Inc. and affiliates
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 package main
 
 import (
@@ -11,11 +30,8 @@ import (
 	"github.com/fatih/color"
 	"github.com/l50/goutils/v2/dev/lint"
 	mageutils "github.com/l50/goutils/v2/dev/mage"
-	"github.com/l50/goutils/v2/docs"
-	"github.com/l50/goutils/v2/git"
 	"github.com/l50/goutils/v2/str"
 	"github.com/l50/goutils/v2/sys"
-	"github.com/spf13/afero"
 
 	// mage utility functions
 
@@ -159,40 +175,6 @@ func RunPreCommit() error {
 	fmt.Println(color.YellowString("Running all pre-commit hooks locally."))
 	if err := lint.RunPCHooks(); err != nil {
 		return err
-	}
-
-	return nil
-}
-
-// GeneratePackageDocs creates documentation for the various packages
-// in the project.
-//
-// Example usage:
-//
-// ```go
-// mage generatepackagedocs
-// ```
-//
-// **Returns:**
-//
-// error: An error if any issue occurs during documentation generation.
-func GeneratePackageDocs() error {
-	fs := afero.NewOsFs()
-
-	repoRoot, err := git.RepoRoot()
-	if err != nil {
-		return fmt.Errorf("failed to get repo root: %v", err)
-	}
-	sys.Cd(repoRoot)
-
-	repo := docs.Repo{
-		Owner: "l50",
-		Name:  "goutils/v2",
-	}
-
-	templatePath := filepath.Join("magefiles", "tmpl", "README.md.tmpl")
-	if err := docs.CreatePackageDocs(fs, repo, templatePath); err != nil {
-		return fmt.Errorf("failed to create package docs: %v", err)
 	}
 
 	return nil
